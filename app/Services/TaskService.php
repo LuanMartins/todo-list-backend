dd8058640ef98432882ffa5c6fb992c66fc96115
Luan martins <luan_18martins@hotmail.com>
2025-06-27 19:15:13 -0300

    feat: create and implement service for task

---
+++ app/Services/TaskService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Dtos\CreateTaskDto;
+use App\Dtos\UpdateTaskDto;
+use App\Repositories\Contracts\PaginatorInterface;
+use App\Repositories\Contracts\TaskRepositoryInterface;
+
+class TaskService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct(private TaskRepositoryInterface $repository)
+    {
+        
+    }
+
+    public function getPaginate(int $page = 1, string $filter = '', int $totalPerPage = 15): PaginatorInterface
+    {
+        return $this->repository->getPaginate(
+            page: $page,
+            filter: $filter,
+            totalPerPage: $totalPerPage
+        );
+    }
+
+
+    public function create(CreateTaskDto $dto): ?object
+    {
+        return $this->repository->createNew($dto);
+    }
+
+
+    public function update(UpdateTaskDto $dto): ?bool
+    {
+        return $this->repository->edit($dto);
+    }
+
+    public function delete(string $id): ?bool
+    {
+        return $this->repository->remove($id);
+    }
+}
