9ecdd146dfcb97c269902fb6d1c982510efe06d8
Luan martins <luan_18martins@hotmail.com>
2025-06-27 20:00:42 -0300

    feat: create function for save task

--- app/Http/Controllers/Api/v1/TaskController.php
+++ app/Http/Controllers/Api/v1/TaskController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Adapters\ResourcePaginatorAdapter;
+use App\Dtos\CreateTaskDto;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateTaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Services\TaskService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class TaskController extends Controller
 {
@@ -22,5 +25,13 @@ class TaskController extends Controller
         );
 
         return ResourcePaginatorAdapter::toJson(TaskResource::class, $tasks);
+    }
+
+
+    public function create(CreateTaskRequest $request): JsonResource
+    {
+        $task = $this->service->create(new CreateTaskDto(...$request->validated()));
+
+        return new TaskResource($task);
     }
 }
