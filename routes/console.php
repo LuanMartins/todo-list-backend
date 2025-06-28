f4e305207823be9228b488668c66178a23c01c77
Luan martins <luan_18martins@hotmail.com>
2025-06-28 07:33:17 -0300

    feat: change run every ten minutes only

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(new DeleteCompletedTasks)->everyMinute();
+Schedule::job(new DeleteCompletedTasks)->everyTenMinutes();
